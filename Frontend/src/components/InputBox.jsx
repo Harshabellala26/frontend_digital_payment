6238772d8e649ea64804f0d0aacc629d8c2965ab
Harshabellala26 <harshabellala23@gmail.com>
2024-12-25 18:03:55 +0530

    Delete Frontend/src directory

--- Frontend/src/components/InputBox.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function InputBox({ label, onchange, placeholder }) {
-  <h3>{label}</h3>;
-  return <input type="text" onChange={onchange} placeholder={placeholder} />;
-}
-
-export default InputBox;
